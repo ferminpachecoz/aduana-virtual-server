4ae4247b41ac9cad1d3811e039aa68d77ff51ff5
ferminpachecoz <88350344+ferminpachecoz@users.noreply.github.com>
2022-06-09 20:28:17 -0300

    database

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../database/models')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +9,11 @@ router.get('/', function(req, res, next) {
 
 router.get('/fercho', function(req, res, next){
   res.status(200).json({saludo: 'que haces fercho todo piola?'})
+})
+
+router.get('/users', function(req, res, next){
+  db.User.findAll()
+    .then(data => res.status(200).json(data))
 })
 
 module.exports = router;
