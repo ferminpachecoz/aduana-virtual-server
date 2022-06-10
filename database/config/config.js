30fe321681bcf8b40512310795301d15bfe694ee
ferminpachecoz <88350344+ferminpachecoz@users.noreply.github.com>
2022-06-09 21:31:19 -0300

    config .env

--- database/config/config.js
+++ database/config/config.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
+
 module.exports={
   "development": {
-    "username": "root",
+    "username": process.env.DB_USER,
     "password": null,
-    "database": "aduana_virtual",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": "mysql",
+    "operatorsAliases": 0
   },
   "test": {
     "username": "root",
